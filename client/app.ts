39f037d464a5dedc66904310c07df46ec769fb2f
Gang Fan <gang.fan@uconn.edu>
2016-07-12 13:33:28 -0400

    Step 3 - 3-Way data binding

--- client/app.ts
+++ client/app.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
- 
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Parties }   from '../collections/parties';
+import { Mongo }     from 'meteor/mongo';
+
 import template from './app.html';
  
 @Component({
@@ -10,22 +12,10 @@ import template from './app.html';
 
 })
 class Socially {
-	parties: Array<Object>;
+	parties: Mongo.Cursor<Object>;
+
 	constructor() {
-	  this.parties = [
-	    {'name': 'Dubstep-Free Zone',
-	      'description': 'Can we please just for an evening not listen to dubstep.',
-	      'location': 'Palo Alto'
-	    },
-	    {'name': 'All dubstep all the time',
-	      'description': 'Get it on!',
-	      'location': 'Palo Alto'
-	    },
-	    {'name': 'Savage lounging',
-	      'description': 'Leisure suit required. And only fiercest manners.',
-	      'location': 'San Francisco'
-	    }
-	  ];
+		this.parties = Parties.find();
 	}
 }
  
