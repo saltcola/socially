6b8cee2a70adcebac0cb4efd96793e432a465bfe
Gang Fan <gang.fan@uconn.edu>
2016-07-13 11:11:14 -0400

    Step 4 - Adding/removing objects and Angular event handling

--- client/app.ts
+++ client/app.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { Parties }   from '../collections/parties';
 import { Mongo }     from 'meteor/mongo';
+import { PartiesForm } from './imports/parties-form/parties-form';
 
 import template from './app.html';
  
 @Component({
   selector: 'app',
   template,
+  directives: [PartiesForm],
 
 })
 class Socially {
@@ -16,6 +18,9 @@ class Socially {
 
 	constructor() {
 		this.parties = Parties.find();
+	}
+	removeParty(party) {
+		Parties.remove(party._id);
 	}
 }
  
